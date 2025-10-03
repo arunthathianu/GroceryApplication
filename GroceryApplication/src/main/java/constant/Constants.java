fa2b75d5bb415fa2cc2f568088ee2942d4f3ad74
arunthathianu <arunthathianu@gmail.com>
2025-10-03 13:58:42 +0530

    Parallel Testing and Cross Browser Testing implemented

--- GroceryApplication/src/main/java/constant/Constants.java
+++ GroceryApplication/src/main/java/constant/Constants.java
@@ -7,5 +7,6 @@ public class Constants {
 	public static final String ADMIN_SHEET="AdminPage";
 	public static final String HOMEPAGE_SHEET="HomePage";
 	public static final String ADMINUSER="Admin";
+	public static final String CONFIGFILEPATH=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
 		
 }
--- GroceryApplication/src/test/java/base/TestNGBase.java
+++ GroceryApplication/src/test/java/base/TestNGBase.java
@@ -1,44 +1,74 @@
 package base;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
+import constant.Constants;
 import utilities.ScreenshotUtility;
 
 public class TestNGBase {
 	
+	Properties prop;
+	FileInputStream f;
 	public WebDriver driver;
 	
-	@BeforeMethod
-	public void initializebrowser()
+	@BeforeMethod(alwaysRun = true)
+	@Parameters("browser")
+	public void initializebrowser(String browser) throws Exception
 	{
-		ChromeOptions options = new ChromeOptions();
-		Map<String,Object> prefs=new HashMap<>();
-		prefs.put("profile.password_manager_leak_detection", false);
-		options.setExperimentalOption("prefs", prefs);
-		driver=new ChromeDriver(options);
+//		ChromeOptions options = new ChromeOptions();
+//		Map<String,Object> prefs=new HashMap<>();
+//		prefs.put("profile.password_manager_leak_detection", false);
+//		options.setExperimentalOption("prefs", prefs);
+//		driver=new ChromeDriver(options);
 		//driver = new ChromeDriver();
 		//driver = new EdgeDriver();
 		//driver = new FirefoxDriver();
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		
+		prop =new Properties();
+		f = new FileInputStream(Constants.CONFIGFILEPATH);
+		prop.load(f);
+		if(browser.equalsIgnoreCase("Chrome")) {
+			//driver=new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			Map<String,Object> prefs=new HashMap<>();
+			prefs.put("profile.password_manager_leak_detection", false);
+			options.setExperimentalOption("prefs", prefs);
+			driver=new ChromeDriver(options);
+		}
+		else if(browser.equalsIgnoreCase("Firefox")) {
+			driver=new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("Edge")) {
+			driver=new EdgeDriver();
+		}
+		else {
+			throw new Exception("Invalid browser name");
+		}
+		driver.get(prop.getProperty("url"));
+		//driver.get("https://groceryapp.uniqassosiates.com/admin/login");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
 	
 	@AfterMethod
 	public void browserclose()
 	{
 		driver.close();	//tab
-		//driver.quit();	//window
+		driver.quit();	//window
 	}
 	
 	@AfterMethod(alwaysRun = true)	
